2f7d3a52e431813d1aef82ffb8fb8ee7edde333a
SaraJourdan65 <144224463+SaraJourdan65@users.noreply.github.com>
2024-03-13 10:58:53 +0100

    Update credentials.example.php

--- idea_box/credentials.example.php
+++ idea_box/credentials.example.php
@@ -1,7 +1,38 @@
-<?php
-  $host = '127.0.0.1';
-  $charset = 'utf8';
-  $user = '';
-  $password = '';
-  $dbname = '';
-?>
+<html>
+  <head>
+    <title>
+      Convertisseur Euro en Dollar
+    </title>
+  </head>
+  <body>
+    <p>
+      <?php
+        if (isset($_GET['euro']))
+        {
+          $cours = 1.0684;
+          $dollar = $_GET['euro'] * $cours;
+          print($_GET['euro'] . " € = $dollar \$.");
+        }
+        else
+        {
+          print("Ajoutez <code>?euro=10</code> à la fin de l'adresse du site.");
+        }
+        if (isset($_GET['dollar']))
+        {
+          $cours = 1.0684;
+          $euro = $_GET['dollar' * $cours;
+          print($_GET['dollar'] . " $ = $euro\€.");
+        }
+        else
+        {
+          print("Ajoutez <code>?dollar=50</code> à la fin de l'adresse du site.");
+        }
+      ?>
+    </p>
+    <hr>
+    <p>
+      La variable <code>$_GET</code> contient <br>
+      <pre><?php var_dump($_GET); ?></pre>
+    </p>
+  </body>
+</html>
